698efbeaf68ee121f5c4ddc018a37de8649017b4
krasko <krasko@localhost>
2014-06-16 00:47:37 +0400

    variable name for adjacency listst fixed

--- src/ru/aptu/dissection_generator/Dissection.java
+++ src/ru/aptu/dissection_generator/Dissection.java
@@ -6,57 +6,57 @@ import java.util.List;
 public class Dissection {
     private int[] polygonSizes;
     private List<Dissection> children = new ArrayList<>();
-    private int[][] incidence;
-
+    private int[][] adjacent;
+    
     private static int[] EMPTY = new int[0];
-
+    
     public Dissection(int size, int[] polygonSizes) {
-        incidence = new int[size][];
+        adjacent = new int[size][];
         for (int i = 0; i < size; ++i) {
-            incidence[i] = EMPTY;
+            adjacent[i] = EMPTY;
         }
         this.polygonSizes = polygonSizes;
     }
-
-
+    
+    
     private Dissection(Dissection parent, int after, int size) {
         this.polygonSizes = parent.polygonSizes;
-        incidence = new int[parent.getOrder() + size - 2][];
-
+        adjacent = new int[parent.getOrder() + size - 2][];
+        
         int[] ptrs = new int[getOrder()];
         int before = (after + size - 1) % getOrder();
-
+        
         for (int i = after + 1; i < after + size - 1; ++i) {
-            incidence[i] = EMPTY;
+            adjacent[i] = EMPTY;
         }
-
+        
         ptrs[after]++;
-
+        
         for (int i = 0; i < parent.getOrder(); ++i) {
             int to = (i <= after ? i : i + size - 2);
-            int sz = parent.incidence[i].length + (to == after || to == before ? 1 : 0);
-            incidence[to] = sz == 0 ? EMPTY : new int[sz];
-            for (int v : parent.incidence[i]) {
-                incidence[to][ptrs[to]++] = (v <= after ? v : v + size - 2);
+            int sz = parent.adjacent[i].length + (to == after || to == before ? 1 : 0);
+            adjacent[to] = sz == 0 ? EMPTY : new int[sz];
+            for (int v : parent.adjacent[i]) {
+                adjacent[to][ptrs[to]++] = (v <= after ? v : v + size - 2);
             }
         }
-
-        incidence[after][0] = before;
-        incidence[before][ptrs[before]++] = after;
+        
+        adjacent[after][0] = before;
+        adjacent[before][ptrs[before]++] = after;
     }
-
+    
     public List<Dissection> getChildren() {
         return children;
     }
-
+    
     public void addChild(Dissection child) {
         children.add(child);
     }
-
+    
     public int[] neighbors(int i) {
-        return incidence[i];
+        return adjacent[i];
     }
-
+    
     public List<UpperDissection> createAllUpper() {
         List<UpperDissection> result = new ArrayList<>();
         for (Integer size : polygonSizes) {
@@ -66,19 +66,19 @@ public class Dissection {
         }
         return result;
     }
-
+    
     public Dissection gluePolygon(int after, int size) {
         return new Dissection(this, after, size);
     }
-
+    
     public boolean isCanonicalRoot(int v) {
         return v % getPeriod() == getFirstCanonicalRoot() % getPeriod();
     }
-
+    
     public int getOrder() {
-        return incidence.length;
+        return adjacent.length;
     }
-
+    
     private int getFirstCanonicalRoot() {
         int best = -1;
         for (int shift = 0; shift < getPeriod(); ++shift) {
@@ -88,22 +88,22 @@ public class Dissection {
         }
         return best;
     }
-
+    
     private boolean canCut(int at) {
-        if (incidence[at].length == 0) return false;
+        if (adjacent[at].length == 0) return false;
         int cur = at;
         do {
             cur = (cur + 1) % getOrder();
-        } while (incidence[cur].length == 0);
-        return incidence[at][0] == cur;
+        } while (adjacent[cur].length == 0);
+        return adjacent[at][0] == cur;
     }
-
+    
     private int compareShifts(int s1, int s2) {
         for (int i = 0; i < getOrder(); ++i) {
-            int[] a1 = incidence[(s1 + i) % getOrder()];
-            int[] a2 = incidence[(s2 + i) % getOrder()];
+            int[] a1 = adjacent[(s1 + i) % getOrder()];
+            int[] a2 = adjacent[(s2 + i) % getOrder()];
             if (a1.length != a2.length) return a1.length - a2.length;
-
+            
             for (int j = 0; j < a1.length; ++j) {
                 int v1 = (a1[j] - s1 + getOrder()) % getOrder();
                 int v2 = (a2[j] - s2 + getOrder()) % getOrder();
@@ -112,9 +112,9 @@ public class Dissection {
         }
         return 0;
     }
-
+    
     private int period = -1;
-
+    
     private int getPeriod() {
         if (period == -1) {
             period = getOrder();
